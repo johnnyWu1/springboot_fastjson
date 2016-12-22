148075ad1b33db124661cc8a97cde8a768d982aa
johnney <842269153@qq.com>
2016-12-22 15:16:42 +0800

    swagger

--- src/main/java/com/jonney/DemoApplication.java
+++ src/main/java/com/jonney/DemoApplication.java
@@ -1,19 +1,22 @@
 package com.jonney;
 
+import java.util.regex.Pattern;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.serializer.ValueFilter;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.google.common.base.Predicate;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.models.dto.builder.ApiInfoBuilder;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -34,5 +37,25 @@ public class DemoApplication {
 			return converter;
 		}
 		
+		@Bean
+	    public Docket swaggerSpringMvcPlugin() {
+	        ApiInfo apiInfo = new ApiInfoBuilder().title("testtitle").description("desc").build();
+			return null;
+	    }
+
+		static private Predicate<String> regex(String string) {
+			return new Predicate<String>() {
+				@Override
+				public boolean apply(String arg0) {
+					return Pattern.matches(string, arg0);
+				}
+			};
+		}
+		
 	}
+
+
+
+
+	
 }
